d3c7be57fe0f560dca151c329c66d1135b466cf4
Ict Support <1705068@kiit.ac.in>
2020-01-27 10:43:54 +0530

    Identifier custom left

---
+++ Lab3/identifier.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef enum _State {
+    S_START,
+    S_LETTER,
+    S_DIGIT,
+    S_INVALID,
+    S_VALID
+} State;
+
+#define S_END S_VALID
+
+// State dfa(State s, char *input) {
+//     if(*input == '\0' || *input == '\n') return s;
+
+//     if(*input == '+' || *input=='-' && s==S_START) return dfa(S_DIGIT, input+1);
+//     else if(*input >='0' && *input<='9' && s==S_START) return dfa(S_END, input+1);
+//     else if(*input >='0' && *input<='9' && s==S_DIGIT) return dfa(S_END, input+1);
+//     else if(*input >='0' && *input<='9' && s==S_VALID) return dfa(S_END, input+1);
+//     else return S_INVALID;
+// }
+
+// int main(){
+//     char ch[1024];
+
+//     printf("Enter string : ");
+//     char buffer[1024];
+//     fgets(buffer, sizeof(buffer), stdin);
+
+//     State s = dfa(S_START, buffer);
+
+//     printf("The given string %s\n", s == S_END ? "is an integer\n" : "is not an integer\n");
+
+//     return 0;
+// }
---
+++ Lab3/sh_identifier.c
@@ -0,0 +1,77 @@
+// By Shubham Kumar, 1705072 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char s[10];
+    printf("\nenter string:. ");
+    scanf("%s",s);
+  	int a = 0;
+  	int count = 0;
+    int index = 0;
+    char ch = s[0];
+    printf("%s",s);
+    
+    // Integer
+    for(int i=0;i<10;i++)
+    {
+    	if(s[i]=='+' || s[i] == '-')
+    	{
+    		while(s[++i]>='0' && s[++i]<='9' )
+    		{
+    			a =1;
+    		}
+    	}
+    	else if (s[i]>='0' && s[i]<='9')
+    	{
+    		a = 1;
+    	}
+    }
+    if(s[0]>='a' && s[0] <= 'z')
+    	{
+    		a =0;
+    		
+    	}
+    if(a==1 )
+    {
+    	printf("\nSatisfy\n");
+    }
+    else
+    {
+    	printf("\nDo not satisfy\n");	
+    }
+    a =0;
+
+
+    
+    // identifier
+    for(int i=0;i<10;i++)
+    {
+    	if(s[i]>='a' && s[i] <= 'z')
+    	{
+    		while(s[i]>='a' && s[i]<='z' )
+    		{
+    			a =1;
+    			while(s[i]>='0' && s[i]<='9' )
+    			{
+    				a = 1;
+    				i++;
+    			}
+    			i++;
+    		}
+    	}
+
+    }
+   
+    if(a==1 )
+    {
+    	printf("\nSatisfy\n");
+    }
+    else
+    {
+    	printf("\nDo not satisfy\n");	
+    }
+
+    return 0;
+}
